1192adc2f2a057e75a6232ec6d46fd874dfa4b0f
thaemilly <thaemillyvitoria211@gmail.com>
2026-06-11 13:58:12 -0300

    Implementação do teste de registro de venda válida

--- Teste.c
+++ Teste.c
@@ -1,3 +1,22 @@
 #include <stdio.h>
 #include "minunit.h"
 #include <math.h>
+
+int regVendas(char *pdt_v, int cate_v, float valor, int qtde_v);
+
+//Registro de Vendas//
+
+MU_TEST(regvenda_valida){
+	mu_assert(regVendas("Camiseta", 1, 50, 2)==1, "Venda deveria ser valida");
+}
+
+
+MU_TEST_SUITE(teste_suite){
+	MU_RUN_TEST(regvenda_valida);
+}
+
+int main(){
+	MU_RUN_SUITE(teste_suite);
+	MU_REPORT();
+	return MU_EXIT_CODE;
+}
--- Teste_minunit.c
+++ Teste_minunit.c
@@ -1,1 +1,14 @@
 #include <math.h>
+
+
+int regVendas(char *pdt_v, int cate_v, float valor, int qtde_v){
+	if(cate_v != 1 && cate_v != 2){
+		return 0;
+	}
+	if(valor < 0 || qtde_v <= 0){
+		return 0;
+	}
+	
+	return 1;
+}
+
